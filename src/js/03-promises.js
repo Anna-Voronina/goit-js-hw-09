a4633d916fca0794f43e7bcc584504e0b10104cc
Anna-Voronina <anna.vs2512@gmail.com>
2023-05-31 21:13:19 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,24 @@
+const formEl = document.querySelector('.form');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
   } else {
-    // Reject
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  }
+}
+
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const delay = event.currentTarget.elements.delay.value;
+  const step = event.currentTarget.elements.step.value;
+  const amount = event.currentTarget.elements.amount.value;
+
+  for (let i = 0; i < amount; i += 1) {
+    createPromise();
   }
 }
