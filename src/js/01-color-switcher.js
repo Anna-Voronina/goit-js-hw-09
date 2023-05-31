5802abe070040dbe31daffaba29c9f38356d5efb
Anna-Voronina <anna.vs2512@gmail.com>
2023-05-31 16:33:49 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const startBtnEl = document.querySelector('button[data-start]');
+const stopBtnEl = document.querySelector('button[data-stop]');
 
+let intervalID = null;
+stopBtnEl.disabled = true;
+
+startBtnEl.addEventListener('click', onStartBtnClick);
+stopBtnEl.addEventListener('click', onStopBtnClick);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function onStartBtnClick() {
+  document.body.style.backgroundColor = getRandomHexColor();
+
+  stopBtnEl.disabled = false;
+  startBtnEl.disabled = true;
+
+  intervalID = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopBtnClick() {
+  clearInterval(intervalID);
+  startBtnEl.disabled = false;
+  stopBtnEl.disabled = true;
+}
